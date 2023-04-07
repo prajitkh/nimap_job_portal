395ec7cdaf09384e0c1f6eec745c42370c3de077
Prajitkhawshi <prajit@nimapinfotech.com>
2023-04-07 18:52:53 +0530

    mockito

--- src/test/java/com/jobportal/Test.java
+++ src/test/java/com/jobportal/Test.java
@@ -1,15 +1,21 @@
 package com.jobportal;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import com.jobportal.repositories.UserRepository;
 import com.jobportal.serviceImpl.UserServiceImpl;
 
+@RunWith(MockitoJUnitRunner.class)
 public class Test {
 	@Mock
 	private UserRepository userRepository;
+
+	private MockMvc mockMvc;
 
 	@InjectMocks
 	private UserServiceImpl userService;
---
+++ src/test/java/com/jobportal/serviceImpl/UserServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.jobportal.serviceImpl;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+class UserServiceImplTest {
+	UserServiceImpl serviceImpl;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		System.out.println("Befor all");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+
+	}
+
+}
