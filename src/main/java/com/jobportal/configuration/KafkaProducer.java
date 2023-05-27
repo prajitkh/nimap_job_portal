e0552e160b89e394d595aed0ae8253f9ccd1b508
Prajitkhawshi <prajit@nimapinfotech.com>
2023-05-27 16:15:15 +0530

    e

--- src/main/java/com/jobportal/configuration/KafkaProducer.java
+++ src/main/java/com/jobportal/configuration/KafkaProducer.java
@@ -1,5 +1,7 @@
 package com.jobportal.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -8,12 +10,15 @@ import org.springframework.stereotype.Service;
 public class KafkaProducer {
 
 	public static final String USER = "userBulk:1:1";
+	private static Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
 
 	@Autowired
 	private KafkaTemplate<String, Object> kafkaTemplate;
 
 	public void addUsersToUsersMainTable(String object) {
 		System.err.println("Kafka producer********");
+		LOGGER.debug("========topic Name===== " + object + "=========message=======" + object);
+
 		kafkaTemplate.send(USER, object);
 
 	}
